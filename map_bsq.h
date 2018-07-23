52df00f5cc8fe6f1770ea6f3240218f597d12827
Eric LI <li.eric.contact@gmail.com>
2018-07-23 14:04:25 +0200

    Correction : Merci Louis

--- map_bsq.h
+++ map_bsq.h
@@ -6,39 +6,39 @@
 /*   By: erli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 10:36:18 by erli              #+#    #+#             */
-/*   Updated: 2018/07/23 13:02:14 by erli             ###   ########.fr       */
+/*   Updated: 2018/07/23 14:04:09 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_BSQ_H
 # define MAP_BSQ_H
 
-typedef	struct	s_list
+typedef	struct		s_list
 {
-	char		c;
-	s_list		*next;
-}				t_list;
+	char			c;
+	struct s_list	*next;
+}					t_list;
 
-typedef	struct	s_coord
+typedef	struct		s_coord
 {
-	int 		x;
-	int 		y;
-}				t_coord;
+	int 			x;
+	int 			y;
+}					t_coord;
 
-typedef	struct	s_global
+typedef	struct		s_global
 {
-	int			nl;
-	int			nc;
-	char 		empty;
-	char		full;
-	char		obs;
-	int			**map;
-}				t_global;
+	int				nl;
+	int				nc;
+	char 			empty;
+	char			full;
+	char			obs;
+	int				**map;
+}					t_global;
 
-typedef	struct	s_sq
+typedef	struct		s_sq
 {
-	t_coord 	*top;
-	t_coord		*bot;
-}				t_sq;
+	t_coord 		*top;
+	t_coord			*bot;
+}					t_sq;
 
 #endif
