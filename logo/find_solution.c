d46ddf6a28f6ba82a433784dace4a2c68627b219
Goki LY <gly@e2r8p12.42.fr>
2018-07-24 12:56:58 +0200

    Correct errors in find_sol & print_sol

--- logo/find_solution.c
+++ logo/find_solution.c
@@ -6,11 +6,13 @@
 /*   By: gly <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 10:49:09 by gly               #+#    #+#             */
-/*   Updated: 2018/07/23 15:13:33 by gly              ###   ########.fr       */
+/*   Updated: 2018/07/24 12:56:33 by gly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map_bsq.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int		valid_place(t_coord pos, int size, int **tab, t_global map_info)
 {
@@ -34,42 +36,47 @@ int		valid_place(t_coord pos, int size, int **tab, t_global map_info)
 		return (!(tab[pos.y + size - 1][pos.x + size - 1]));
 }
 
-t_sq	fill_sol(t_coord tmp, int size)
+t_sq	*fill_sol(t_coord tmp, int size, t_sq *sol)
 {
 	t_coord	*top;
 	t_coord	*bot;
-	t_sq	sol;
 
+	if (!(top = malloc(sizeof(*top))))
+		return (sol);
+	if (!(bot = malloc(sizeof(*bot))))
+		return (sol);
+	bot = 0;
 	top->x = tmp.x;
 	top->y = tmp.y;
 	bot->x = tmp.x + size - 1;
 	bot->y = tmp.y + size - 1;
-	sol.top = top;
-	sol.bot = bot;
+	sol->top = top;
+	sol->bot = bot;
 	return (sol);
 }
 
-t_sq	find_solution(int **tab, t_global map_info)
+t_sq	*find_solution(int **tab, t_global map_info)
 {
 	int		size;
 	t_coord	tmp;
-	t_sq	sol;
+	t_sq	*sol;
 
 	tmp.y = 0;
 	size = 1;
-	while (tmp.y < map_info.nl)
+	while (tmp.y + size < map_info.nl)
 	{
 		tmp.x = 0;
-		while (tmp.x < map_info.nc)
+		while (tmp.x + size < map_info.nc)
 		{
 			while (valid_place(tmp, size, tab, map_info))
 			{
-				sol = fill_sol(tmp, size);
+				sol = fill_sol(tmp, size, sol);
 				size++;
 			}
 			tmp.x++;
 		}
 		tmp.y++;
 	}
+	printf("Size of the square is %i\n", size);
 	return (sol);
 }
--- logo/print_solution.c
+++ logo/print_solution.c
@@ -6,20 +6,36 @@
 /*   By: gly <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/23 13:12:08 by gly               #+#    #+#             */
-/*   Updated: 2018/07/23 13:58:36 by gly              ###   ########.fr       */
+/*   Updated: 2018/07/24 12:55:45 by gly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map_bsq.h"
 #include "proto.h"
+#include <stdio.h>
+
+int		is_obs(t_coord pos, int **tab)
+{
+	if (pos.x >= 1)
+	{
+		if (pos.y >= 1)
+			return (tab[pos.y - 1][pos.x - 1] - tab[pos.y][pos.x - 1] -
+						tab[pos.y - 1][pos.x] + tab[pos.y][pos.x]);
+		else
+			return (tab[pos.y][pos.x] -	tab[pos.y][pos.x - 1]);
+	}
+	else if (pos.y >= 1)
+		return (tab[pos.y][pos.x] - tab[pos.y - 1][pos.x]);
+	else
+		return (tab[pos.y][pos.x]);
+}
 
 void	print_pos(t_coord tmp, int **tab, t_sq sol, t_global map)
 {
 	if (tmp.x >= sol.top->x && tmp.x <= sol.bot->x &&
 			tmp.y >= sol.top->y && tmp.y <= sol.bot->y)
 		ft_putchar(map.full);
-	else if (tab[tmp.y][tmp.x] - tab[tmp.y - 1][tmp.x - 1] -
-			tab[tmp.y - 1][tmp.x] + tab[tmp.y - 1][tmp.x - 1])
+	else if (!is_obs(tmp, tab))
 		ft_putchar(map.empty);
 	else
 		ft_putchar(map.obs);
@@ -35,6 +51,7 @@ void	print_solution(t_sq sol, t_global map_info)
 	tmp.x = 0;
 	while (tmp.y < map_info.nc)
 	{
+		printf("Line %i", tmp.y);
 		tmp.x = 0;
 		while (tmp.x < map_info.nl)
 		{
@@ -42,5 +59,6 @@ void	print_solution(t_sq sol, t_global map_info)
 			tmp.x++;
 		}
 		tmp.y++;
+		ft_putchar('\n');
 	}
 }
